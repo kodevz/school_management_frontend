878c0291de9d2e1db535c05f82518ea44b7b595a
kodevz <karthi.php.developer@gmail.com>
2020-11-13 21:29:14 +0530

    Environment Production File Changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  clientSecret: 'RjoSYkYmRdT7zGRUBVrJrmc3FrHTaUmJAfkeC0bA',
+  clientId: 2,
+  grantType: 'password',
 };
+
+export const ApiHosts = {
+  oauth: 'http://127.0.0.1:8000/oauth/',
+  api: 'http://127.0.0.1:8000/api/v1/',
+  web: 'http://127.0.0.1:8000/'
+}
